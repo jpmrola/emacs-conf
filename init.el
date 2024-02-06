48a70b5019b1611721d6c446e6f8f79b8d918f7c
João Rõla <jpmrola@gmail.com>
2024-02-06 00:16:49 +0000

    update config

--- init.el
+++ init.el
@@ -1,1 +1,18 @@
 (org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "34cf3305b35e3a8132a0b1bdf2c67623bc2cb05b125f8d7d26bd51fd16d547ec" default))
+ '(ispell-dictionary nil)
+ '(package-selected-packages
+   '(yaml-mode dockerfile-mode xref shrface which-key vterm visual-fill-column use-package undo-tree typescript-mode treemacs-tab-bar treemacs-projectile treemacs-magit treemacs-evil sublimity smooth-scrolling smartparens rust-mode ranger rainbow-delimiters pdf-tools org-bullets minimap lsp-ui lsp-treemacs lsp-ivy ivy-rich helpful general evil-collection doom-themes doom-modeline dired-single dired-open counsel-projectile company-box command-log-mode cmake-mode auto-package-update all-the-icons-dired)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'dired-find-alternate-file 'disabled nil)
