8f53ec80cdb1bec28603e99a3d230df722d2816e
João Rõla <jpmrola@gmail.com>
2025-05-30 15:07:18 +0100

    Sync with PC

--- init.el
+++ init.el
@@ -5,10 +5,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "34cf3305b35e3a8132a0b1bdf2c67623bc2cb05b125f8d7d26bd51fd16d547ec" default))
+   '("2b20b4633721cc23869499012a69894293d49e147feeb833663fdc968f240873" "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "b5fd9c7429d52190235f2383e47d340d7ff769f141cd8f9e7a4629a81abc6b19" "9f297216c88ca3f47e5f10f8bd884ab24ac5bc9d884f0f23589b0a46a608fe14" "f5f80dd6588e59cfc3ce2f11568ff8296717a938edd448a947f9823a4e282b66" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "34cf3305b35e3a8132a0b1bdf2c67623bc2cb05b125f8d7d26bd51fd16d547ec" default))
  '(ispell-dictionary nil)
  '(package-selected-packages
-   '(yaml-mode dockerfile-mode xref shrface which-key vterm visual-fill-column use-package undo-tree typescript-mode treemacs-tab-bar treemacs-projectile treemacs-magit treemacs-evil sublimity smooth-scrolling smartparens rust-mode ranger rainbow-delimiters pdf-tools org-bullets minimap lsp-ui lsp-treemacs lsp-ivy ivy-rich helpful general evil-collection doom-themes doom-modeline dired-single dired-open counsel-projectile company-box command-log-mode cmake-mode auto-package-update all-the-icons-dired)))
+   '(mu4e wallabag anki yaml-mode dockerfile-mode xref shrface which-key vterm visual-fill-column use-package undo-tree typescript-mode treemacs-tab-bar treemacs-projectile treemacs-magit treemacs-evil sublimity smooth-scrolling smartparens rust-mode ranger rainbow-delimiters pdf-tools org-bullets minimap lsp-ui lsp-treemacs lsp-ivy ivy-rich helpful general evil-collection doom-themes doom-modeline dired-single dired-open counsel-projectile company-box command-log-mode cmake-mode auto-package-update all-the-icons-dired))
+ '(package-vc-selected-packages
+   '((vc-use-package :vc-backend Git :url "https://github.com/slotThe/vc-use-package"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
